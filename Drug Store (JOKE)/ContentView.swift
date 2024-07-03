265b5165e5e67104d55268753e7c5bf6c9aecceb
contyadvait <advaitthepro@outlook.com>
2024-07-03 16:29:27 +0800

    Initial Commit

---
+++ Drug Store (JOKE)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drug Store (JOKE)
+//
+//  Created by Milind Contractor on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drug Store (JOKE)/Drug_Store__JOKE_App.swift
@@ -0,0 +1,17 @@
+//
+//  Drug_Store__JOKE_App.swift
+//  Drug Store (JOKE)
+//
+//  Created by Milind Contractor on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Drug_Store__JOKE_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
